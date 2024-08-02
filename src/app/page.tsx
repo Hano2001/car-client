8325d9158626d071f73cfdeed8d5080d1d8183ed
lukas-laas <lukas.s.laas@gmail.com>
2024-08-02 10:50:09 +0200

    display cars from db
    Co-authored-by: Orianamendez <egleemendez@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,25 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
+type Car = { id: string; status: string };
+
 export default async function Home() {
-  const cars = await GetAllCars();
-  console.log({ cars });
+  const cars: Car[] = await GetAllCars();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section>
         <RadioGroup defaultValue="option-one">
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="option-one" id="option-one" />
-            <Label htmlFor="option-one">Option One</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="option-two" id="option-two" />
-            <Label htmlFor="option-two">Option Two</Label>
-          </div>
+          {cars.map((car) => {
+            return car.status == "available" ? (
+              <div className="flex items-center space-x-2" key={car.id}>
+                <RadioGroupItem value="option-two" id={car.id} />
+                <Label htmlFor="option-two">{car.status}</Label>
+              </div>
+            ) : (
+              <></>
+            );
+          })}
         </RadioGroup>
         <Button>¡BUY!</Button>
       </section>
