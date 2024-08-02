e34db3db7e8b3361eea91feebc12d8eaf895e1c1
lukas-laas <lukas.s.laas@gmail.com>
2024-08-02 10:03:33 +0200

    add shadcn radio group

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <header>
+        <h1>¡Los mejores carros!</h1>
+      </header>
+      <section>
+        <RadioGroup defaultValue="option-one">
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="option-one" id="option-one" />
+            <Label htmlFor="option-one">Option One</Label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="option-two" id="option-two" />
+            <Label htmlFor="option-two">Option Two</Label>
+          </div>
+        </RadioGroup>
+        <Button>¡BUY!</Button>
+      </section>
+    </main>
   );
 }
